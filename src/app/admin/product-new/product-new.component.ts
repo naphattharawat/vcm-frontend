c22cf9fadf4c1ecfeeb71e7ab2be3d1de98973e5
naphattharawat <naphattharawat@gmail.com>
2019-01-06 23:36:23 +0700

    add product success

--- src/app/admin/product-new/product-new.component.ts
+++ src/app/admin/product-new/product-new.component.ts
@@ -1,3 +1,5 @@
+import { AlertService } from './../../alert.service';
+import { ProductsService } from './../products.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductNewComponent implements OnInit {
 
-  constructor() { }
+  file: Array<File>;
+  fileName: any;
+
+  productNameTH: any;
+  productNameEN: any;
+  model: any;
+  serial: any;
+  unit: any;
+  costOther: any;
+  costOtherCurrency: any;
+  cost: any;
+  price: any;
+  keyword: any;
+  note: any;
+  constructor(
+    private productsService: ProductsService,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
   }
+
+  onFileChanged(e: any) {
+    this.file = <Array<File>>e.target.files;
+    // this.file.
+    // console.log(this.file);
+
+    this.fileName = this.file[0].name;
+    // const fileName = this.file[0].name;
+  }
+
+  async save() {
+    const data = {
+      product_name_th: this.productNameTH,
+      product_name_en: this.productNameEN,
+      serial: this.serial,
+      model: this.model,
+      unit: this.unit,
+      cost_other: this.costOther,
+      cost_other_currency: this.costOtherCurrency,
+      cost: this.cost,
+      price: this.price,
+      keyword: this.keyword,
+      note: this.note,
+      qty: 0,
+      status: 1
+    };
+    const rs: any = await this.productsService.save(data);
+    if (rs.ok) {
+      const productId = rs.rows[0];
+      if (this.file) {
+        console.log(this.file);
+
+        await this.productsService.uploadFile(productId, this.file, this.fileName);
+      }
+
+    } else {
+      this.alertService.error(rs.error);
+    }
+  }
+
 
 }
--- src/app/admin/products.service.ts
+++ src/app/admin/products.service.ts
@@ -3,12 +3,17 @@ import { AuthHttp } from 'angular2-jwt';
 import { HttpClient } from '@angular/common/http'
 import 'rxjs/add/operator/toPromise';
 
+
 @Injectable()
 export class ProductsService {
+  token: any;
   constructor(
     @Inject('API_URL') private url: string,
     private http: HttpClient
-  ) { }
+  ) {
+    this.token = sessionStorage.getItem('token');
+  }
+
 
   async search(query: any, limit: number = 10, offset: number = 0) {
     const resp = await this.http.post(`${this.url}/products/search`, {
@@ -23,6 +28,40 @@ export class ProductsService {
     const resp = await this.http.post(`${this.url}/products`, {
       limit: limit,
       offset: offset
+    }).toPromise();
+    return resp;
+  }
+
+  uploadFile(productId: string, files: Array<File>, fileName: string = null) {
+    return new Promise((resolve, reject) => {
+      const formData: any = new FormData();
+      const xhr = new XMLHttpRequest();
+      for (let i = 0; i < files.length; i++) {
+        formData.append('files[]', files[i], fileName);
+      }
+      formData.append('productId', productId);
+      formData.append('fileName', fileName);
+      formData.append('token', this.token);
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      };
+
+      const url = `${this.url}/products/image?token=${this.token}`;
+      xhr.open('POST', url, true);
+      xhr.send(formData);
+    });
+  }
+
+  async save(data) {
+    const resp = await this.http.post(`${this.url}/products/save`, {
+      data: data
     }).toPromise();
     return resp;
   }
